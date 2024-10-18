438138dfd1ac781978cd4263cb72dba4313e94ce
Nicolas David Orjuela Sanchez <norjuelas@unal.edu.co>
2024-10-18 14:25:30 -0500

    Fixing price error

--- src/airbnb_scraper.py
+++ src/airbnb_scraper.py
@@ -43,52 +43,91 @@ class AirbnbScraper:
         """Extrae los datos de las tarjetas en la página actual utilizando BeautifulSoup."""
         cards_data = []
         try:
-            # Obtener el HTML de la página actual
-            page_source = self.driver.page_source
-            soup = BeautifulSoup(page_source, 'html.parser')
-
-            # Encontrar todas las tarjetas de listado
-            cards = soup.find_all('div', class_='cy5jw6o')
+            soup = self.get_soup()  # Llamada a la función para obtener el BeautifulSoup
+            cards = self.find_cards(soup)  # Llamada a la función para encontrar las cards
             print(f"Encontrados {len(cards)} cards en la página.")
+            
             for card in cards:
-                try:
-                    link_component = card.find('a', class_='bn2bl2p')
-                    TypeDescription = card.find('div', class_='t1jojoys')
-                    description = card.find('div', class_='s1cjsi4j')
-                    image = card.find('img', class_='itu7ddv')
-                    price = card.find('span', class_='_1aejdbt')
-                    rating = card.find('span', class_='r4a59j5')
-                    if link_component and 'href' in link_component.attrs:
-                        idPublication = urlparse(link_component['href']).path.split('/')[-1]
-                    else:
-                        idPublication = None
-
-                    print(link_component)
-                    lat, lon = self.extract_lat_lon(idPublication)
-                    print(idPublication, lat, lon)
-
-                    data = {
-                        "link": link_component['href'] if link_component else None,
-                        "TypeDescription": TypeDescription.get_text(strip=True) if TypeDescription else None,
-                        "description": description.get_text(strip=True) if description else None,
-                        "image": image['src'] if image else None,
-                        "price": price.get_text(strip=True) if price else None,
-                        "rating": rating.get_text(strip=True) if rating else None,
-                        "idPublication": idPublication if idPublication else None,
-                        "lat": lat if lat else None,
-                        "lon": lon if lon else None,
-                        "TypeRoomOrHouse": self.roomOrHouse(TypeDescription.get_text(strip=True))
-
-                    }
-
-                    cards_data.append(data)
-
-                except Exception as e:
-                    print(f"Error al extraer un card: {e}")
-                    continue
+                card_data = self.extract_card_data(card)
+                if card_data:
+                    cards_data.append(card_data)
+        
         except Exception as e:
             print(f"Error al encontrar las cards: {e}")
+        
         return cards_data
+
+
+    def get_soup(self):
+        """Obtiene el HTML de la página actual y lo convierte en BeautifulSoup."""
+        try:
+            page_source = self.driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
+            return soup
+        except Exception as e:
+            print(f"Error al obtener el HTML: {e}")
+            return None
+
+
+    def find_cards(self, soup):
+        """Encuentra todas las tarjetas de listados (cards) en la página."""
+        try:
+            return soup.find_all('div', class_='cy5jw6o')
+        except Exception as e:
+            print(f"Error al encontrar las tarjetas de listado: {e}")
+            return []
+
+
+    def extract_card_data(self, card):
+        """Extrae los datos de una tarjeta individual."""
+        try:
+            link_component, TypeDescription, description, image, price, rating, idPublication = self.parse_card(card)
+            
+            # Obtener latitud y longitud
+            lat, lon = self.extract_lat_lon(idPublication)
+            print(idPublication, lat, lon)
+            
+            # Crear diccionario con la información extraída
+            data = {
+                "link": link_component['href'] if link_component else None,
+                "TypeDescription": TypeDescription.get_text(strip=True) if TypeDescription else None,
+                "description": description.get_text(strip=True) if description else None,
+                "image": image['src'] if image else None,
+                "price": price.get_text(strip=True) if price else None,
+                "rating": rating.get_text(strip=True) if rating else None,
+                "idPublication": idPublication if idPublication else None,
+                "lat": lat if lat else None,
+                "lon": lon if lon else None,
+                "TypeRoomOrHouse": self.roomOrHouse(TypeDescription.get_text(strip=True)) if TypeDescription else None
+            }
+            return data
+        
+        except Exception as e:
+            print(f"Error al extraer un card: {e}")
+            return None
+
+
+    def parse_card(self, card):
+        """Parsea una tarjeta y extrae los componentes individuales."""
+        try:
+            link_component = card.find('a', class_='bn2bl2p')
+            TypeDescription = card.find('div', class_='t1jojoys')
+            description = card.find('div', class_='s1cjsi4j')
+            image = card.find('img', class_='itu7ddv')
+            price = card.find('span', class_='_11jcbg2')
+            rating = card.find('span', class_='r4a59j5')
+            
+            # Obtener idPublication del link
+            if link_component and 'href' in link_component.attrs:
+                idPublication = urlparse(link_component['href']).path.split('/')[-1]
+            else:
+                idPublication = None
+            
+            return link_component, TypeDescription, description, image, price, rating, idPublication
+        
+        except Exception as e:
+            print(f"Error al parsear la tarjeta: {e}")
+            return None, None, None, None, None, None, None
 
     def extract_next_links(self):
         """Extrae los enlaces para las próximas páginas desde la página actual."""
@@ -172,77 +211,7 @@ class AirbnbScraper:
                 print('no hay cordenada pa')
                 attempts += 1
         return 0 , 0
-
-
-    def save_to_csv(self, filename="airbnb_listings.csv"):
-        """
-        Guarda el DataFrame acumulado en un archivo CSV, añadiendo los nuevos datos sin sobrescribir,
-        eliminando duplicados y actualizando el historial de precios.
-        """
-        # Verificar si el archivo CSV ya existe
-        if os.path.exists(filename):
-            # Leer los datos existentes, asegurando que se interprete 'price_history' como listas
-            existing_df = pd.read_csv(filename, converters={'price_history': ast.literal_eval})
-            original_size = existing_df.shape[0]
-            print(f"\nEl archivo existente tiene {original_size} registros.")
-        else:
-            # Crear un DataFrame vacío si el archivo no existe
-            existing_df = pd.DataFrame()
-            original_size = 0
-            print("\nNo se encontró archivo existente. Se creará uno nuevo.")
-        
-        current_date = time.strftime('%Y-%m-%d %H:%M:%S')
-        self.master_df['last_read_date'] = current_date
-
-        # Verificar o crear la columna de historial de precios en el DataFrame existente
-        if 'price_history' not in existing_df.columns:
-            existing_df['price_history'] = [[] for _ in range(existing_df.shape[0])]
-        if 'TypeRoomOrHouse' not in existing_df.columns:
-            self.filterRoomOrHouse(existing_df)
-
-        # Actualizar el historial de precios
-        for i, row in self.master_df.iterrows():
-            # Buscar si el idPublication ya existe en el DataFrame existente
-            matching_row = existing_df[existing_df['idPublication'] == row['idPublication']]
-
-            # Si existe, actualizamos el historial de precios
-            if not matching_row.empty:
-                index = matching_row.index[0]
-                price_history = existing_df.at[index, 'price_history']
-
-                # Agregar siempre la nueva tupla (precio actual, fecha actual) al historial
-                new_entry = (row['price'], current_date)
-                price_history.append(new_entry)
-
-                # Actualizar el DataFrame con el nuevo historial de precios
-                existing_df.at[index, 'price_history'] = price_history
-
-            # Si no existe, crear una nueva entrada
-            else:
-                new_price_history = [(row['price'], current_date)]
-                row['price_history'] = new_price_history
-                existing_df = pd.concat([existing_df, pd.DataFrame([row])], ignore_index=True)
-
-        # Eliminar duplicados basados en la columna 'idPublication'
-        combined_df = existing_df.drop_duplicates(subset=['idPublication'], keep='last')
-
-        # Calcular el tamaño final del DataFrame y la cantidad de nuevos registros añadidos
-        final_size = combined_df.shape[0]
-        new_data_count = final_size - original_size
-
-        print(f"\nDatos actuales analizados: {self.master_df.shape[0]}")
-        print(f"Nuevos datos añadidos: {new_data_count}")
-        print(f"El DataFrame ha crecido de {original_size} a {final_size} registros.")
-
-        # Guardar el DataFrame actualizado en CSV
-        combined_df.to_csv(filename, index=False)
-        file_path = os.path.join(os.getcwd(), filename)
-        print(f"Datos exportados a: {file_path}")
-
-        # Actualizar el atributo residuales
-        self.residuales = new_data_count
-
-
+    
     def roomOrHouse(self,TypeDescription:str)->str:
         """
         clasifica descripciones si es habitacion o apto
@@ -307,3 +276,68 @@ class AirbnbScraper:
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"\nTiempo total de ejecución: {elapsed_time:.2f} segundos")
+
+    def save_to_csv(self, filename="airbnb_listings.csv"):
+        existing_df, original_size = self.verificar_archivo_existente(filename)
+        existing_df = self.inicializar_dataframe(existing_df)
+        current_date = time.strftime('%Y-%m-%d %H:%M:%S')
+        #existing_df = self.actualizar_historial_precios(existing_df, current_date)
+        combined_df = self.eliminar_duplicados(existing_df)
+        self.actualizar_estadisticas(original_size, combined_df)
+        self.guardar_dataframe(combined_df, filename)
+
+    def actualizar_estadisticas(self, original_size, combined_df):
+        final_size = combined_df.shape[0]
+        new_data_count = final_size - original_size
+        print(f"Nuevos datos añadidos: {new_data_count}")
+        print(f"El DataFrame ha crecido de {original_size} a {final_size} registros.")
+        self.residuales = new_data_count
+
+
+    def guardar_dataframe(self, combined_df, filename):
+        combined_df.to_csv(filename, index=False)
+        file_path = os.path.join(os.getcwd(), filename)
+        print(f"Datos exportados a: {file_path}")
+
+    def eliminar_duplicados(self, existing_df):
+        return existing_df.drop_duplicates(subset=['idPublication'], keep='last')
+
+    def actualizar_historial_precios(self, existing_df, current_date):
+        for i, row in self.master_df.iterrows():
+            matching_row = existing_df[existing_df['idPublication'] == row['idPublication']]
+            
+            if not matching_row.empty:
+                index = matching_row.index[0]
+                price_history = existing_df.at[index, 'price_history']
+                new_entry = (row['price'], current_date)
+                price_history.append(new_entry)
+                existing_df.at[index, 'price_history'] = price_history
+            else:
+                new_price_history = [(row['price'], current_date)]
+                row['price_history'] = new_price_history
+                existing_df = pd.concat([existing_df, pd.DataFrame([row])], ignore_index=True)
+
+        return existing_df
+
+    def inicializar_dataframe(self, existing_df):
+        current_date = time.strftime('%Y-%m-%d %H:%M:%S')
+        self.master_df['last_read_date'] = current_date
+
+        #if 'price_history' not in existing_df.columns:
+            #existing_df['price_history'] = [[] for _ in range(existing_df.shape[0])]
+        #if 'TypeRoomOrHouse' not in existing_df.columns:
+            #self.filterRoomOrHouse(existing_df)
+        
+        return existing_df
+
+    def verificar_archivo_existente(self, filename):
+        if os.path.exists(filename):
+            existing_df = pd.read_csv(filename, converters={'price_history': ast.literal_eval})
+            original_size = existing_df.shape[0]
+            print(f"\nEl archivo existente tiene {original_size} registros.")
+        else:
+            existing_df = pd.DataFrame()
+            original_size = 0
+            print("\nNo se encontró archivo existente. Se creará uno nuevo.")
+        return existing_df, original_size
+
